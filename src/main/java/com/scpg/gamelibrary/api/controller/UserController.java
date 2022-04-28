58d7b792295e8a5c89f306901cc763c87efaf1da
Emre Berat Kıran <doctoremre7@gmail.com>
2022-04-28 17:13:51 +0300

    - Broken Generic Approach

--- src/main/java/com/scpg/gamelibrary/api/controller/UserController.java
+++ src/main/java/com/scpg/gamelibrary/api/controller/UserController.java
@@ -1,49 +1,21 @@
 package com.scpg.gamelibrary.api.controller;
 
 import com.scpg.gamelibrary.business.abstracts.IUserService;
-import com.scpg.gamelibrary.entities.concretes.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController @RequestMapping("api/users")
+@RestController
 @RequiredArgsConstructor
-public class UserController
+public abstract class UserController<T, S extends IUserService<T>>
 {
-    private final IUserService userService;
-
+    protected final S genericUserService;
 
     @GetMapping("/getall")
-    public List<User> getAll()
+    public List<T> getAll()
     {
-        return this.userService.getAll();
-    }
-
-    @GetMapping("/getbyid")
-    public User getById(@RequestParam int id)
-    {
-        return this.userService.getById(id);
-    }
-
-    @GetMapping("/getbyusername")
-    public User getByUsername(@RequestParam String username)
-    {
-        return this.userService.getByUsername(username);
-    }
-
-    @GetMapping("/register")
-    public User Register(@RequestParam int id, @RequestParam String name, @RequestParam String password)
-    {
-        return userService.add(new User(id, name, password));
-    }
-
-    @GetMapping("/deletebyid")
-    public void DeleteById(@RequestParam int id)
-    {
-        userService.remove(userService.getById(id));
+        return genericUserService.getAll();
     }
 }
